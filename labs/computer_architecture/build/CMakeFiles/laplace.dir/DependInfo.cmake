8361517b48e92cf3f6b2c043f1c7252d91219052
Edward Glockner <edward.glockner5@gmail.com>
2023-08-15 15:39:45 +0200

    adding files

---
+++ labs/computer_architecture/build/CMakeFiles/laplace.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/edwardglockner/OneDrive - Uppsala universitet/PDC/labs/computer_architecture/main.cpp" "CMakeFiles/laplace.dir/main.cpp.o" "gcc" "CMakeFiles/laplace.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ labs/computer_architecture/build/CMakeFiles/laplace.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/laplace.dir/main.cpp.o"
+  "CMakeFiles/laplace.dir/main.cpp.o.d"
+  "laplace"
+  "laplace.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/laplace.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
